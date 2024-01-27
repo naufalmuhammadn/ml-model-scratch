ae7a45eb86ff924161c8ca6bfc0ced076ac7c98b
naufalmuhammadn <naufalmuhammadmail@gmail.com>
2024-01-27 12:36:25 +0700

    feat: add KNN base model

---
+++ scratch/neighbors/__init__.py
@@ -0,0 +1,9 @@
+from ._base import NearestNeighbor
+from ._regressor import KNeighborsRegressor
+from ._classifier import KNeighborsClassifier
+
+__all__ = [
+    "NearestNeighbor",
+    "KNeighborsRegressor",
+    "KNeighborsClassifier"
+]
